59f5c01544dda317c37a5a7b0072d21c2f7c0e86
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-08-01 11:29:07 +0200

    Feat: isNumber and isString

--- src/utils/is-type.ts
+++ src/utils/is-type.ts
@@ -1,1 +1,3 @@
 export const isFunction = (value: any): value is Function => typeof value === 'function';
+export const isString = (value: any): value is string => typeof value === 'string';
+export const isNumber = (value: any): value is number => typeof value === 'number';
